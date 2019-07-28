dd6d9ef9dcdd1dc8de48894a9a104f48f6e109c2
akysil <tyoma.ksl@gmail.com>
2019-07-29 00:23:10 +0300

    v1.0.1

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector('.header__btn');
+const topLine = document.querySelector('.header__btn-line--top');
+const line = document.querySelector('.header__btn-line--center');
+const bottomLine = document.querySelector('.header__btn-line--bottom');
+const menu = document.querySelector('.header__menu-list');
+
+btn.addEventListener('click', () => {
+   line.classList.toggle('hide');
+   topLine.classList.toggle('rotate-l');
+   bottomLine.classList.toggle('rotate-r');
+   menu.classList.toggle('active');
+   setTimeout(function () {
+       menu.classList.toggle('slide')
+   }, 200);
+});
+
+// btn.addEventListener('mouseout', () => {
+//    line.classList.remove('hide');
+//    topLine.classList.remove('rotate-l');
+//    bottomLine.classList.remove('rotate.r');
+// });
